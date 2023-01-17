9456991b20929d80f5c9ccffee1e4fe0f419161d
Long Do <dohonglong1998@gmail.com>
2023-01-17 14:25:50 +0200

    Replace localhost with render site

--- client/src/custom-hooks/getDrones.js
+++ client/src/custom-hooks/getDrones.js
@@ -9,11 +9,14 @@ const useDrones = () => {
   useEffect(() => {
     const fetchDrones = async () => {
       try {
-        await fetch("http://localhost:4050/express_backend", {
-          headers: {
-            "Content-Type": "application/xml; charset=utf-8",
-          },
-        })
+        await fetch(
+          "https://reaktor-drone-fullstack.onrender.com/express_backend",
+          {
+            headers: {
+              "Content-Type": "application/xml; charset=utf-8",
+            },
+          }
+        )
           .then((response) => response.text())
           .then((data) => {
             console.log(data);
@@ -35,47 +38,3 @@ const useDrones = () => {
 };
 
 export default useDrones;
-
-// useEffect(() => {
-//   axios("https://assignments.reaktor.com/birdnest/drones", {
-//     mode: "cors",
-//     headers: {
-//       //proxy: "http://localhost:3000",
-//       "Access-Control-Allow-Origin": "localhost",
-//       "Content-Type": "application/xml; charset=utf-8",
-//       "Access-Control-Allow-Credentials": "true",
-//     },
-//   })
-//     .then((response) => {
-//       console.log(response);
-//       setDrones(response.data);
-//     })
-//     .catch((error) => {
-//       setError(error);
-//     });
-// }, []);
-
-// const response = await fetch(
-//   "https://assignments.reaktor.com/birdnest/drones",
-//   {
-//     mode: "no-cors",
-//     headers: {
-//       "Content-Type": "application/xml; charset=utf-8",
-//     },
-//   }
-// );
-// const data = await response.text();
-// var xml = new XMLParser().parseFromString(data);
-// console.log(xml);
-// setDrones(xml);
-
-//const data = await response.text();
-//console.log(data);
-//var xml = new XMLParser().parseFromString(data);
-//setDrones(xml);
-
-// .then(res => res.text())
-// .then(data => {
-//     var xml = new XMLParser().parseFromString(data);
-//     console.log(xml)
-// })
